1957bb113d6506b46ce631bdd9ae04826980d96d
Felipe Alencar <felype99azevedo@gmail.com>
2025-12-20 19:23:51 -0300

    finish `api/status/` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log({ result: result.rows });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query({ text: "SHOW server_version;" });
+  const databaseMaxConnections = await database.query({ text: "SHOW max_connections;" });
 
-  response.status(200).json({ message: "Serviço up - 200" });
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnections.rows[0].max_connections),
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      }
+    }
+  });
 }
 
 export default status;
